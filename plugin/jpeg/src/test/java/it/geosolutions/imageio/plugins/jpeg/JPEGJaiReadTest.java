2b1999b58bfa29d1c3ecab1fbdc2eead2d0fb8b1
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2007-09-17 22:14:18 +0000

    -adding license headers -cleaning tests a bit
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@32 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- plugin/jpeg/src/test/java/it/geosolutions/imageio/plugins/jpeg/JPEGJaiReadTest.java
+++ plugin/jpeg/src/test/java/it/geosolutions/imageio/plugins/jpeg/JPEGJaiReadTest.java
@@ -1,18 +1,19 @@
 /*
-* JImageIO-extension - OpenSource Java Image translation Library
-* http://www.geo-solutions.it/
-* (C) 2007, GeoSolutions
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation;
-* version 2.1 of the License.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*/
+ *    JImageIO-extension - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *	  https://imageio-ext.dev.java.net/
+ *    (C) 2007, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package it.geosolutions.imageio.plugins.jpeg;
 
 import it.geosolutions.imageio.gdalframework.Viewer;
@@ -52,7 +53,10 @@ public class JPEGJaiReadTest extends AbstractJPEGTestCase {
 		pbjImageRead.setParameter("readParam", irp);
 
 		RenderedOp image = JAI.create("ImageRead", pbjImageRead);
-		Viewer.visualize(image);
+		if(TestData.isInteractiveTest())
+			Viewer.visualize(image);
+		else
+			image.getTiles();
 	}
 	
 	public static Test suite() {
