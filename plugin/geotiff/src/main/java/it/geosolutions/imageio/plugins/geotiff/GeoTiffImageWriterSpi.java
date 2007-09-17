93bbc542d47b3a1a9cae26a6d562995920122e2d
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2007-09-17 22:10:30 +0000

    -improved geotiff tests -added license headers
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@31 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- plugin/geotiff/src/main/java/it/geosolutions/imageio/plugins/geotiff/GeoTiffImageWriterSpi.java
+++ plugin/geotiff/src/main/java/it/geosolutions/imageio/plugins/geotiff/GeoTiffImageWriterSpi.java
@@ -1,18 +1,19 @@
 /*
-* JImageIO-extension - OpenSource Java Image translation Library
-* http://www.geo-solutions.it/
-* (C) 2007, GeoSolutions
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation;
-* version 2.1 of the License.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*/
+ *    JImageIO-extension - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *	  https://imageio-ext.dev.java.net/
+ *    (C) 2007, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package it.geosolutions.imageio.plugins.geotiff;
 
 import it.geosolutions.imageio.gdalframework.GDALImageWriterSpi;
