f5b4f76f2f2363daa99397bfdb3d976075aabd16
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2007-09-24 22:18:46 +0000

    -improving test for imagereadmt
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@68 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- library/imagereadmt/src/test/java/it/geosolutions/imageio/imageioimpl/imagereadmt/ImageIOExtTest.java
+++ library/imagereadmt/src/test/java/it/geosolutions/imageio/imageioimpl/imagereadmt/ImageIOExtTest.java
@@ -16,9 +16,6 @@
  */
 package it.geosolutions.imageio.imageioimpl.imagereadmt;
 
-import it.geosolutions.imageio.imageioimpl.imagereadmt.ImageReadDescriptorMT;
-
-import javax.media.jai.JAI;
 import javax.media.jai.ParameterBlockJAI;
 
 import junit.framework.TestCase;
@@ -50,16 +47,13 @@ public class ImageIOExtTest extends TestCase {
 		super.tearDown();
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		TestRunner.run(ImageIOExtTest.class);
 	}
 
-	public void testImageReadMT(){
-		final JAI jai = JAI.getDefaultInstance();
-		ImageReadDescriptorMT.register(jai);
-		
+	public void testImageReadMT() {
+
 		final ParameterBlockJAI pbj = new ParameterBlockJAI("ImageReadMT");
-		
 		assertNotNull(pbj);
 	}
 }
