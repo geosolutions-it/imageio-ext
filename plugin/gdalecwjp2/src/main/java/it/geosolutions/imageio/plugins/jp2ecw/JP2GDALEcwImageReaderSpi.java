5eed748a9fee443a2db9dde647c7d1f4c07e27b2
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2007-10-09 13:30:13 +0000

    - added jp2000 (ecw based) plugin
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@129 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdalecwjp2/src/main/java/it/geosolutions/imageio/plugins/jp2ecw/JP2GDALEcwImageReaderSpi.java
@@ -0,0 +1,167 @@
+/*
+ *    JImageIO-extension - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *	  https://imageio-ext.dev.java.net/
+ *    (C) 2007, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.jp2ecw;
+
+import it.geosolutions.imageio.gdalframework.GDALImageReaderSpi;
+import it.geosolutions.imageio.gdalframework.GDALUtilities;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageReader;
+import javax.imageio.spi.ImageReaderSpi;
+import javax.imageio.spi.ServiceRegistry;
+
+/**
+ * Service provider interface for the jp2k image
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions.
+ */
+public class JP2GDALEcwImageReaderSpi extends GDALImageReaderSpi {
+
+	private static final Logger logger = Logger
+			.getLogger("it.geosolutions.imageio.plugins.jp2ecw");
+
+	static final String[] suffixes = { "jp2", "jp2" };
+
+	static final String[] formatNames = { "JPEG2000" };
+
+	static final String[] MIMETypes = { "image/jp2", "image/jp2k" };
+
+	static final String version = "1.0";
+
+	static final String readerCN = "it.geosolutions.imageio.plugins.jp2ecw.JP2GDALEcwImageReader";
+
+	static final String vendorName = "GeoSolutions";
+
+	// writerSpiNames
+	static final String[] wSN = { null };
+
+	// StreamMetadataFormatNames and StreamMetadataFormatClassNames
+	static final boolean supportsStandardStreamMetadataFormat = false;
+
+	static final String nativeStreamMetadataFormatName = null;
+
+	static final String nativeStreamMetadataFormatClassName = null;
+
+	static final String[] extraStreamMetadataFormatNames = { null };
+
+	static final String[] extraStreamMetadataFormatClassNames = { null };
+
+	// ImageMetadataFormatNames and ImageMetadataFormatClassNames
+	static final boolean supportsStandardImageMetadataFormat = false;
+
+	static final String nativeImageMetadataFormatName = null;
+
+	static final String nativeImageMetadataFormatClassName = null;
+
+	static final String[] extraImageMetadataFormatNames = { null };
+
+	static final String[] extraImageMetadataFormatClassNames = { null };
+
+	private boolean registered;
+
+	public JP2GDALEcwImageReaderSpi() {
+		super(
+				vendorName,
+				version,
+				formatNames,
+				suffixes,
+				MIMETypes,
+				readerCN, // readerClassName
+				STANDARD_INPUT_TYPE,
+				wSN, // writer Spi Names
+				supportsStandardStreamMetadataFormat,
+				nativeStreamMetadataFormatName,
+				nativeStreamMetadataFormatClassName,
+				extraStreamMetadataFormatNames,
+				extraStreamMetadataFormatClassNames,
+				supportsStandardImageMetadataFormat,
+				nativeImageMetadataFormatName,
+				nativeImageMetadataFormatClassName,
+				extraImageMetadataFormatNames,
+				extraImageMetadataFormatClassNames);
+
+		if (logger.isLoggable(Level.FINE))
+			logger.fine("JP2GDALEcwImageReaderSpi Constructor");
+		needsTileTuning = true;
+	}
+
+	/**
+	 * This method checks if the provided input can be decoded from this SPI
+	 */
+	public boolean canDecodeInput(Object input) throws IOException {
+		return super.canDecodeInput(input);
+	}
+
+	/**
+	 * Returns an instance of the JP2GDALEcwImageReader
+	 * 
+	 * @see javax.imageio.spi.ImageReaderSpi#createReaderInstance(java.lang.Object)
+	 */
+	public ImageReader createReaderInstance(Object source) throws IOException {
+		return new JP2GDALEcwImageReader(this);
+	}
+
+	/**
+	 * @see javax.imageio.spi.IIOServiceProvider#getDescription(java.util.Locale)
+	 */
+	public String getDescription(Locale locale) {
+		return new StringBuffer("JP2K Image Reader, version ").append(version)
+				.toString();
+	}
+
+	protected String getSupportedFormats() {
+		return "JP2ECW";
+	}
+
+	/**
+	 * Upon registration, this method ensures that this SPI is listed at the top
+	 * of the ImageReaderSpi items, so that it will be invoked before the
+	 * default ImageReaderSpi
+	 * 
+	 * @param registry
+	 *            ServiceRegistry where this object has been registered.
+	 * @param category
+	 *            a Class object indicating the registry category under which
+	 *            this object has been registered.
+	 */
+	public void onRegistration(ServiceRegistry registry, Class category) {
+		super.onRegistration(registry, category);
+		if (registered) {
+			return;
+		}
+
+		registered = true;
+
+		Iterator readers = GDALUtilities.getJDKImageReaderWriterSPI(registry,
+				"JPEG2000", true).iterator();
+
+		ImageReaderSpi spi;
+		while (readers.hasNext()) {
+			spi = (ImageReaderSpi) readers.next();
+			if (spi == this)
+				continue;
+			registry.deregisterServiceProvider(spi);
+			registry.setOrdering(category, this, spi);
+		}
+	}
+}
---
+++ plugin/gdalecwjp2/src/test/java/it/geosolutions/imageio/plugins/jp2ecw/JP2KReadTest.java
@@ -0,0 +1,220 @@
+/*
+ *    JImageIO-extension - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *	  https://imageio-ext.dev.java.net/
+ *    (C) 2007, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.jp2ecw;
+
+import it.geosolutions.imageio.gdalframework.Viewer;
+import it.geosolutions.resources.TestData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageReadParam;
+import javax.media.jai.JAI;
+import javax.media.jai.ParameterBlockJAI;
+import javax.media.jai.RenderedOp;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Testing reading capabilities for {@link JP2GDALEcwImageReader} leveraging on
+ * JAI.
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions.
+ */
+public class JP2KReadTest extends AbstractJP2KTestCase {
+
+	private static final Logger LOGGER = Logger
+			.getLogger("it.geosolutions.imageio.plugins.jp2ecw");
+
+	public final static String fileName = "sample.jp2";
+
+	public JP2KReadTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Simple test read
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public void testRead() throws FileNotFoundException, IOException {
+		final ParameterBlockJAI pbjImageRead;
+		final File file = TestData.file(this, fileName);
+		pbjImageRead = new ParameterBlockJAI("ImageRead");
+		pbjImageRead.setParameter("Input", file);
+
+		RenderedOp image = JAI.create("ImageRead", pbjImageRead);
+		if (TestData.isInteractiveTest())
+			Viewer.visualize(image);
+		else
+			assertNotNull(image.getTiles());
+	}
+
+	/**
+	 * Test Mean Running Time
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public void testSpeed() throws FileNotFoundException, IOException {
+		long totalTime = 0;
+		final int nLoops = 5;
+		LOGGER.info("Running Speed test");
+		for (int i = 0; i < nLoops; i++) {
+			long startTime = System.currentTimeMillis();
+			final ParameterBlockJAI pbjImageRead;
+			final File file = TestData.file(this, fileName);
+			pbjImageRead = new ParameterBlockJAI("ImageRead");
+			pbjImageRead.setParameter("Input", file);
+
+			RenderedOp image = JAI.create("ImageRead", pbjImageRead);
+			image.getTiles();
+			long endTime = System.currentTimeMillis();
+			totalTime += endTime - startTime;
+		}
+		LOGGER.info("Mean Running Time " + Long.toString(totalTime / nLoops)
+				+ " milliseconds (" + nLoops + " runs)");
+	}
+
+	/**
+	 * Test read exploiting common JAI operations (Crop-Translate-Rotate)
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public void testJaiOperations() throws IOException {
+		final File inputFile = TestData.file(this, fileName);
+
+		// ////////////////////////////////////////////////////////////////
+		// preparing to read
+		// ////////////////////////////////////////////////////////////////
+		final ParameterBlockJAI pbjImageRead;
+		final ImageReadParam irp = new ImageReadParam();
+
+		Integer xSubSampling = new Integer(1);
+		Integer ySubSampling = new Integer(1);
+		Integer xSubSamplingOffset = new Integer(0);
+		Integer ySubSamplingOffset = new Integer(0);
+
+		irp.setSourceSubsampling(xSubSampling.intValue(), ySubSampling
+				.intValue(), xSubSamplingOffset.intValue(), ySubSamplingOffset
+				.intValue());
+
+		pbjImageRead = new ParameterBlockJAI("ImageRead");
+		pbjImageRead.setParameter("Input", inputFile);
+		pbjImageRead.setParameter("readParam", irp);
+		RenderedOp image = JAI.create("ImageRead", pbjImageRead);
+
+		if (TestData.isInteractiveTest())
+			Viewer.visualize(image, "subsampled");
+
+		// ////////////////////////////////////////////////////////////////
+		// preparing to crop
+		// ////////////////////////////////////////////////////////////////
+		final ParameterBlockJAI pbjCrop = new ParameterBlockJAI("Crop");
+		pbjCrop.addSource(image);
+
+		// Setting a square crop to avoid blanks zone when rotating.
+		Float xCrop = new Float(image.getMinX() + image.getWidth() / 4);
+		Float yCrop = new Float(image.getMinX() + image.getWidth() / 4);
+		Float cropWidth = new Float(image.getWidth() / 4);
+		Float cropHeigth = new Float(image.getWidth() / 4);
+		pbjCrop.setParameter("x", xCrop);
+		pbjCrop.setParameter("y", yCrop);
+		pbjCrop.setParameter("width", cropWidth);
+		pbjCrop.setParameter("height", cropHeigth);
+		final RenderedOp croppedImage = JAI.create("Crop", pbjCrop);
+
+		if (TestData.isInteractiveTest())
+			Viewer.visualize(croppedImage, "cropped");
+
+		// ////////////////////////////////////////////////////////////////
+		// preparing to translate
+		// ////////////////////////////////////////////////////////////////
+		final ParameterBlockJAI pbjTranslate = new ParameterBlockJAI(
+				"Translate");
+		pbjTranslate.addSource(croppedImage);
+		Float xTrans = new Float(xCrop.floatValue() * (-1));
+		Float yTrans = new Float(yCrop.floatValue() * (-1));
+		pbjTranslate.setParameter("xTrans", xTrans);
+		pbjTranslate.setParameter("yTrans", yTrans);
+		final RenderedOp translatedImage = JAI
+				.create("Translate", pbjTranslate);
+
+		if (TestData.isInteractiveTest())
+			Viewer.visualize(translatedImage, "translated");
+
+		// ////////////////////////////////////////////////////////////////
+		// preparing to rotate
+		// ////////////////////////////////////////////////////////////////
+		final ParameterBlockJAI pbjRotate = new ParameterBlockJAI("Rotate");
+		pbjRotate.addSource(translatedImage);
+
+		Float xOrigin = new Float(cropWidth.floatValue() / 2);
+		Float yOrigin = new Float(cropHeigth.floatValue() / 2);
+		Float angle = new Float(java.lang.Math.PI / 2);
+
+		pbjRotate.setParameter("xOrigin", xOrigin);
+		pbjRotate.setParameter("yOrigin", yOrigin);
+		pbjRotate.setParameter("angle", angle);
+
+		final RenderedOp rotatedImage = JAI.create("Rotate", pbjRotate);
+
+		StringBuffer title = new StringBuffer("SUBSAMP:").append("X[").append(
+				xSubSampling.toString()).append("]-Y[").append(
+				ySubSampling.toString()).append("]-Xof[").append(
+				xSubSamplingOffset.toString()).append("]-Yof[").append(
+				ySubSamplingOffset).append("]CROP:X[").append(xCrop.toString())
+				.append("]-Y[").append(yCrop.toString()).append("]-W[").append(
+						cropWidth.toString()).append("]-H[").append(
+						cropHeigth.toString()).append("]TRANS:X[").append(
+						xTrans.toString()).append("]-Y[").append(
+						yTrans.toString()).append("]ROTATE:xOrig[").append(
+						xOrigin.toString()).append("]-yOrig[").append(
+						yOrigin.toString()).append("]-ang[").append(
+						angle.toString()).append("]");
+		if (TestData.isInteractiveTest())
+			Viewer.visualize(rotatedImage, title.toString());
+		else
+			assertNotNull(rotatedImage.getTiles());
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+
+		// Test read exploiting common JAI operations (Crop-Translate-Rotate)
+		suite.addTest(new JP2KReadTest("testJaiOperations"));
+
+		// Test reading of a simple image
+		suite.addTest(new JP2KReadTest("testRead"));
+
+		// Test reading of a simple image to compute mean running time
+		suite.addTest(new JP2KReadTest("testSpeed"));
+
+		return suite;
+	}
+
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+}
