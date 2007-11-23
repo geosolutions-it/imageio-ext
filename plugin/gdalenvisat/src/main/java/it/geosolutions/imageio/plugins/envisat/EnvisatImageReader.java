bc4bafe865de1de8fa0307ee05750e1d693f7b69
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2007-11-23 14:03:09 +0000

    - added support for ESAT format (Envisat)
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@169 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdalenvisat/src/main/java/it/geosolutions/imageio/plugins/envisat/EnvisatImageReader.java
@@ -0,0 +1,84 @@
+/*
+ *    JImageIO-extension - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *	  https://imageio-ext.dev.java.net/
+ *    (C) 2007, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.envisat;
+
+import it.geosolutions.imageio.gdalframework.GDALCommonIIOImageMetadata;
+import it.geosolutions.imageio.gdalframework.GDALImageReader;
+
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.metadata.IIOMetadata;
+
+import org.gdal.gdal.Dataset;
+
+/**
+ * {@link EnvisatImageReader} is a {@link GDALImageReader} able to create
+ * {@link RenderedImage} from Envisat files.
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions. 
+ */
+public class EnvisatImageReader extends GDALImageReader {
+
+	private static final Logger LOGGER = Logger
+			.getLogger("it.geosolutions.imageio.plugins.envisat");
+
+	public class EnvisatDataSetWrapper extends GDALDatasetWrapper {
+		public EnvisatDataSetWrapper(String sDatasetName) {
+			super(sDatasetName);
+		}
+
+		public EnvisatDataSetWrapper(Dataset ds, String name) {
+			super(ds, name);
+		}
+	}
+	
+	public EnvisatImageReader(EnvisatImageReaderSpi originatingProvider) {
+		super(originatingProvider);
+		if (LOGGER.isLoggable(Level.FINE))
+			LOGGER.fine("EnvisatImageReader Constructor");
+		nSubdatasets = 0;
+	}
+
+	public IIOMetadata getImageMetadata(int imageIndex) throws IOException {
+		return getDataSetWrapper(imageIndex).getImageIOMetadata();
+	}
+
+	public IIOMetadata getStreamMetadata() throws IOException {
+		if (LOGGER.isLoggable(Level.INFO))
+			LOGGER
+					.info("This method actually returns null. Use getGDALImageMetadata.");
+		throw new UnsupportedOperationException(
+				"This method actually returns null. Use getGDALStreamMetadata.");
+	}
+
+	protected GDALDatasetWrapper createDataSetWrapper(Dataset mainDataset,
+			String mainDatasetFileName) {
+		return new EnvisatDataSetWrapper(mainDataset, mainDatasetFileName);
+	}
+
+	protected GDALDatasetWrapper createDataSetWrapper(String string) {
+		return new EnvisatDataSetWrapper(string);
+	}
+
+	protected IIOMetadata getIIOImageMetadata(GDALDatasetWrapper wrapper) {
+		return new GDALCommonIIOImageMetadata(wrapper);
+	}
+}
