7705e2f3a0b2e4315fdf50e89e5818bfc0f97597
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2009-06-30 06:57:19 +0000

    Added writer code
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@910 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/kakadujp2/src/main/java/it/geosolutions/imageio/plugins/jp2k/JP2KKakaduImageWriteParam.java
@@ -0,0 +1,167 @@
+/* JP2K Kakadu Image Writer V. 1.0 
+ * 
+ * (c) 2008 Quality Nighthawk Teleradiology Group, Inc.
+ * Contact: info@qualitynighthawk.com
+ *
+ * Produced by GeoSolutions, Eng. Daniele Romagnoli and Eng. Simone Giannecchini
+ * GeoSolutions S.A.S. ---  Via Carignoni 51, 55041 Camaiore (LU) Italy
+ * Contact: info@geo-solutions.it
+ *
+ * Released under the Gnu Lesser General Public License version 3. 
+ * All rights otherwise reserved. 
+ *
+ * JP2K Kakadu Image Writer is distributed on an "AS IS" basis, 
+ * WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * See the GNU Lesser General Public License version 3 for more details. 
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
+package it.geosolutions.imageio.plugins.jp2k;
+
+import javax.imageio.ImageWriteParam;
+
+/**
+ * @author Daniele Romagnoli, GeoSolutions
+ * @author Simone Giannecchini, GeoSolutions
+ */
+public class JP2KKakaduImageWriteParam extends ImageWriteParam {
+
+    /**
+     * Default Constructor.
+     */
+    public JP2KKakaduImageWriteParam() {
+        cLevels = DEFAULT_C_LEVELS;
+        quality = DEFAULT_QUALITY;
+        qualityLayers = 1;
+        canWriteCompressed = true;
+        compressionMode = MODE_EXPLICIT;
+    }
+
+    /**
+     * Specifies whether write only the jpeg2000 code stream. The output will be
+     * a raw codestream.
+     */
+    private boolean writeCodeStreamOnly = false;
+
+    /**
+     * Specifies the quality of the output image as a compression ratio. As an
+     * instance, a value of 0.2 means a 5:1 compression ratio.
+     * 
+     */
+    private double quality;
+    
+    /**
+     * The default number of decomposition levels.
+     */
+    final static double DEFAULT_QUALITY = 1.0;
+
+    /**
+     * Specify the number of quality layers
+     */
+    private int qualityLayers;
+
+    /**
+     * Specify the number of decompositions levels.
+     */
+    private int cLevels;
+    
+    /**
+     * The default number of decomposition levels.
+     */
+    final static int DEFAULT_C_LEVELS = 7;
+
+    /**
+     * Sets <code>writeCodeStreamOnly</code>.
+     * 
+     * @param value
+     *                Whether the jpeg2000 code stream only or the jp2 format
+     *                will be written into the output.
+     * @see #isWriteCodeStreamOnly()
+     */
+    public void setWriteCodeStreamOnly(boolean writeCodeStreamOnly) {
+        this.writeCodeStreamOnly = writeCodeStreamOnly;
+    }
+
+    /**
+     * Gets <code>writeCodeStreamOnly</code>.
+     * 
+     * @return whether the jpeg2000 code stream only or the jp2 format will be
+     *         written into the output.
+     * @see #setWriteCodeStreamOnly(boolean)
+     */
+    public boolean isWriteCodeStreamOnly() {
+        return writeCodeStreamOnly;
+    }
+
+    /**
+     * Gets <code>quality</code>.
+     * 
+     * @return the quality parameter.
+     * 
+     * @see #setQuality(double)
+     */
+    public double getQuality() {
+        return quality;
+    }
+
+    /**
+     * Sets <code>quality</code>.
+     * 
+     * @param quality
+     *                a quality parameter representing a compression ratio. As
+     *                an instance, a 0.2 quality represents a 5:1 compression
+     *                ratio.
+     * 
+     * @see #getQuality()
+     */
+    public void setQuality(final double quality) {
+        this.quality = quality;
+    }
+
+    /**
+     * Gets <code>cLevels</code>.
+     * 
+     * @return the number of decomposition levels.
+     * 
+     * @see #setCLevels(int)
+     */
+    public int getCLevels() {
+        return cLevels;
+    }
+
+    /**
+     * Sets <code>cLevels</code>.
+     * 
+     * @param cLevels
+     *                the number of decomposition levels.
+     * 
+     * @see #getCLevels()()
+     */
+    public void setCLevels(final int cLevels) {
+        this.cLevels = cLevels;
+    }
+
+    /**
+     * Gets <code>qualityLayers</code>.
+     * 
+     * @return the number of quality layers.
+     * 
+     * @see #setQualityLayers(int)
+     */
+    public int getQualityLayers() {
+        return qualityLayers;
+    }
+
+    /**
+     * Sets <code>qualityLayers</code>.
+     * 
+     * @param qualityLayers
+     *                the number of quality layers.
+     * 
+     * @see #getQualityLayers()
+     */
+    public void setQualityLayers(final int qualityLayers) {
+        this.qualityLayers = qualityLayers;
+    }
+}
---
+++ plugin/kakadujp2/src/main/java/it/geosolutions/imageio/plugins/jp2k/JP2KKakaduImageWriterSpi.java
@@ -0,0 +1,114 @@
+/* JP2K Kakadu Image Writer V. 1.0 
+ * 
+ * (c) 2008 Quality Nighthawk Teleradiology Group, Inc.
+ * Contact: info@qualitynighthawk.com
+ *
+ * Produced by GeoSolutions, Eng. Daniele Romagnoli and Eng. Simone Giannecchini
+ * GeoSolutions S.A.S. ---  Via Carignoni 51, 55041 Camaiore (LU) Italy
+ * Contact: info@geo-solutions.it
+ *
+ * Released under the Gnu Lesser General Public License version 3. 
+ * All rights otherwise reserved. 
+ *
+ * JP2K Kakadu Image Writer is distributed on an "AS IS" basis, 
+ * WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * See the GNU Lesser General Public License version 3 for more details. 
+ * http://www.fsf.org/licensing/licenses/lgpl.html
+ */
+package it.geosolutions.imageio.plugins.jp2k;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriter;
+import javax.imageio.spi.ImageWriterSpi;
+
+/**
+ * @author Daniele Romagnoli, GeoSolutions
+ * @author Simone Giannecchini, GeoSolutions
+ */
+public class JP2KKakaduImageWriterSpi extends ImageWriterSpi {
+
+    static final String[] suffixes = { "JP2", "J2C" };
+
+    static final String[] formatNames = { "jpeg2000", "jpeg 2000", "JPEG2000", "JPEG 2000", "JP2", "JP2K" };
+
+    static final String[] MIMETypes = { "image/jp2" };
+
+    static final String version = "1.0";
+
+    static final String writerCN = "it.geosolutions.imageio.plugins.jp2k.JP2kKakaduImageWriter";
+
+    static final String vendorName = "GeoSolutions";
+
+    // ReaderSpiNames
+    static final String[] readerSpiName = { "it.geosolutions.imageio.plugins.jp2k.JP2kKakaduImageReaderSpi" };
+
+    // StreamMetadataFormatNames and StreamMetadataFormatClassNames
+    static final boolean supportsStandardStreamMetadataFormat = false;
+
+    static final String nativeStreamMetadataFormatName = null;
+
+    static final String nativeStreamMetadataFormatClassName = null;
+
+    static final String[] extraStreamMetadataFormatNames = null;
+
+    static final String[] extraStreamMetadataFormatClassNames = null;
+
+    // ImageMetadataFormatNames and ImageMetadataFormatClassNames
+    static final boolean supportsStandardImageMetadataFormat = false;
+
+    static final String nativeImageMetadataFormatName = null;
+
+    static final String nativeImageMetadataFormatClassName = null;
+
+    static final String[] extraImageMetadataFormatNames = { null };
+
+    static final String[] extraImageMetadataFormatClassNames = { null };
+
+    /**
+     * Default {@link ImageWriterSpi} constructor for JP2K writers.
+     */
+    public JP2KKakaduImageWriterSpi() {
+        super(vendorName, version, formatNames, suffixes, MIMETypes, writerCN,
+                STANDARD_OUTPUT_TYPE, readerSpiName,
+                supportsStandardStreamMetadataFormat,
+                nativeStreamMetadataFormatName,
+                nativeStreamMetadataFormatClassName,
+                extraStreamMetadataFormatNames,
+                extraStreamMetadataFormatClassNames,
+                supportsStandardImageMetadataFormat,
+                nativeImageMetadataFormatName,
+                nativeImageMetadataFormatClassName,
+                extraImageMetadataFormatNames,
+                extraImageMetadataFormatClassNames);
+    }
+
+    /**
+     * @see javax.imageio.spi.ImageWriterSpi#createWriterInstance(java.lang.Object)
+     */
+    public ImageWriter createWriterInstance(Object extension)
+            throws IOException {
+        return new JP2KKakaduImageWriter(this);
+    }
+
+    /**
+     * @see javax.imageio.spi.IIOServiceProvider#getDescription(java.util.Locale)
+     */
+    public String getDescription(Locale locale) {
+        return "SPI for JPEG 2000 ImageWriter based on KDU JNI";
+    }
+
+    /**
+     * Refine the check if needed.
+     */
+    public boolean canEncodeImage(ImageTypeSpecifier type) {
+//        final int numBands = type.getNumBands();
+//        final int numBits = type.getBitsPerBand(0);
+        return true;
+    }
+
+}
