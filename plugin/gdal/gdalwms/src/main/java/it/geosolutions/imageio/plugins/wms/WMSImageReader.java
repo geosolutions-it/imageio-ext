a8153295c910a0d148634c83bf944c7ba34fc896
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2009-07-27 16:39:01 +0000

    Adding WMS plugin
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@967 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdal/gdalwms/src/main/java/it/geosolutions/imageio/plugins/wms/WMSImageReader.java
@@ -0,0 +1,42 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    https://imageio-ext.dev.java.net/
+ *    (C) 2007 - 2009, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.wms;
+
+import it.geosolutions.imageio.gdalframework.GDALImageReader;
+
+import java.awt.image.RenderedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * {@link WMSImageReader} is a {@link GDALImageReader} able to create
+ * {@link RenderedImage} from WMS services.
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions.
+ */
+public class WMSImageReader extends GDALImageReader {
+
+    private static final Logger LOGGER = Logger
+            .getLogger("it.geosolutions.imageio.plugins.wms");
+
+    public WMSImageReader(WMSImageReaderSpi originatingProvider) {
+        super(originatingProvider, 0);
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("WMSImageReader Constructor");
+    }
+}
