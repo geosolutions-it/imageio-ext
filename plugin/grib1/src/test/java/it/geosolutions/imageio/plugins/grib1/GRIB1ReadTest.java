d175d158063b9bc4864be941c67c8740f5d09a2b
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2009-07-27 13:47:40 +0000

    Cleaning grib1 tests
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@963 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- plugin/grib1/src/test/java/it/geosolutions/imageio/plugins/grib1/GRIB1ReadTest.java
+++ plugin/grib1/src/test/java/it/geosolutions/imageio/plugins/grib1/GRIB1ReadTest.java
@@ -17,6 +17,7 @@ package it.geosolutions.imageio.plugins.grib1;
 
 import it.geosolutions.imageio.ndplugin.BaseImageMetadata;
 import it.geosolutions.imageio.utilities.ImageIOUtilities;
+import it.geosolutions.resources.TestData;
 
 import java.awt.image.RenderedImage;
 import java.io.File;
@@ -27,26 +28,16 @@ import java.util.logging.Logger;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 
-public class GRIB1ReadTest  {
+import org.junit.Assert;
+
+public class GRIB1ReadTest {
 
     private static final Logger LOGGER = Logger
             .getLogger("it.geosolutions.imageio.plugins.grib1");
 
-    private void warningMessage() {
-        StringBuffer sb = new StringBuffer(
-                "Test file not available. Test are skipped");
-        LOGGER.info(sb.toString());
-    }
+    final static String dataPathPrefix = "y:/data/grib/";
 
-//    final static String fileName = "wrf_NMM_2009022513_operativo_d01.grb";
-//
-    final static String dataPathPrefix = "y:/data/grib/lammatest/nmm_ecm_4km/";
-
-    final static String fileName = "wrf_NMM_2009022512_operativo_d01.grb";
-
-//    final static String dataPathPrefix = "y:/data/grib/Griblibrary_testfolder3/";
-//    final static String dataPathPrefix = "y:/data/grib/TESTALL/";
-
+    final static String fileName = "sample.grb";
 
     /**
      * Simple read from a Grib File containing a lot of records
@@ -55,38 +46,39 @@ public class GRIB1ReadTest  {
      */
     @org.junit.Test
     public void testReadSingleFile() throws IOException {
-//    	File dir = TestData.file(this, "."); 
-    	File dir =	new File(dataPathPrefix);
-		File[] files = dir.listFiles(new FileFilter() {
-			public boolean accept(File pathname) {
-				final String path = pathname.getAbsolutePath();
-				if (path.endsWith(".grib")||path.endsWith(".grb"))
-					return true;
-				return false;
-				
-			}
-		});
-		for (File inputFile : files) {
-			
-	        final ImageReader reader = new GRIB1ImageReaderSpi()
-	                .createReaderInstance();
-	        reader.setInput(inputFile);
-	        final int nImages = reader.getNumImages(false);
-	        for (int index=0;index<nImages;index++){
-		        final ImageReadParam param = new ImageReadParam();
-		        param.setSourceSubsampling(2, 2, 0, 0);
-		
-		        RenderedImage ri = reader.read(index, param);
-	//	        if (TestData.isInteractiveTest())
-		            ImageIOUtilities.visualize(ri);
-	//	        else
-	//	            Assert.assertNotNull(ri.getData());
-		        ImageIOUtilities.displayImageIOMetadata(reader.getImageMetadata(index)
-		                .getAsTree(BaseImageMetadata.nativeMetadataFormatName));
-		        ImageIOUtilities.displayImageIOMetadata(reader.getImageMetadata(index)
-		                .getAsTree(GRIB1ImageMetadata.nativeMetadataFormatName));
-	        }
-	        reader.dispose();
-		}
+        File dir = TestData.file(this, ".");
+        // File dir = new File(dataPathPrefix);
+        File[] files = dir.listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+                final String path = pathname.getAbsolutePath();
+                if (path.endsWith(".grib") || path.endsWith(".grb"))
+                    return true;
+                return false;
+            }
+        });
+        for (File inputFile : files) {
+
+            final ImageReader reader = new GRIB1ImageReaderSpi()
+                    .createReaderInstance();
+            reader.setInput(inputFile);
+            final int nImages = reader.getNumImages(false);
+            for (int index = 0; index < nImages; index++) {
+                final ImageReadParam param = new ImageReadParam();
+                param.setSourceSubsampling(2, 2, 0, 0);
+
+                RenderedImage ri = reader.read(index, param);
+                if (TestData.isInteractiveTest())
+                    ImageIOUtilities.visualize(ri);
+                else
+                    Assert.assertNotNull(ri.getData());
+                ImageIOUtilities.displayImageIOMetadata(reader
+                        .getImageMetadata(index).getAsTree(
+                                BaseImageMetadata.nativeMetadataFormatName));
+                ImageIOUtilities.displayImageIOMetadata(reader
+                        .getImageMetadata(index).getAsTree(
+                                GRIB1ImageMetadata.nativeMetadataFormatName));
+            }
+            reader.dispose();
+        }
     }
 }
