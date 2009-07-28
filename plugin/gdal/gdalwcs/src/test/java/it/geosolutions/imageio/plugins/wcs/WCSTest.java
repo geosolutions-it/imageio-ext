5f6e105aeb1189e76af1ea30bc9a29cfa04badc8
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2009-07-28 15:01:14 +0000

    Adding WCS plugin
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@969 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdal/gdalwcs/src/test/java/it/geosolutions/imageio/plugins/wcs/WCSTest.java
@@ -0,0 +1,81 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    https://imageio-ext.dev.java.net/
+ *    (C) 2007 - 2009, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.wcs;
+
+import it.geosolutions.imageio.gdalframework.AbstractGDALTest;
+import it.geosolutions.imageio.gdalframework.GDALUtilities;
+import it.geosolutions.imageio.plugins.wcs.WCSImageReader;
+import it.geosolutions.imageio.plugins.wcs.WCSImageReaderSpi;
+import it.geosolutions.imageio.utilities.ImageIOUtilities;
+import it.geosolutions.resources.TestData;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.ImageReader;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Testing reading capabilities for {@link WCSImageReader}.
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions.
+ */
+public class WCSTest extends AbstractGDALTest {
+    public final static String fileName = "wcs.xml";
+
+    private final static boolean isDriverAvailable = isGDALAvailable
+            && GDALUtilities.isDriverAvailable("WCS");
+
+    /**
+     * Test read exploiting common JAI operations (Crop-Translate-Rotate)
+     * 
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    @Test
+    public void read() throws FileNotFoundException, IOException {
+        if (!isDriverAvailable) {
+            return;
+        }
+        File file;
+        try {
+            file = TestData.file(this, fileName);
+        } catch (FileNotFoundException fnfe) {
+            warningMessage();
+            return;
+        }
+        // ////////////////////////////////////////////////////////////////
+        // preparing to read
+        // ////////////////////////////////////////////////////////////////
+        final ImageReader mReader = new WCSImageReaderSpi()
+                .createReaderInstance();
+        mReader.setInput(file);
+        final RenderedImage image = mReader.read(0);
+ 
+        if (TestData.isInteractiveTest()) {
+            ImageIOUtilities.visualize(image);
+        }
+        else
+            Assert.assertNotNull(image.getData());
+    }
+
+}
