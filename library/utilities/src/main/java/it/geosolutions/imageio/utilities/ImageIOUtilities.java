1f58e09a9d326d073a8768660bea0aca33721355
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2011-03-29 08:57:27 +0000

    Fixing IMAGEIO_EXT-24: UrlToFile
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@1240 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- library/utilities/src/main/java/it/geosolutions/imageio/utilities/ImageIOUtilities.java
+++ library/utilities/src/main/java/it/geosolutions/imageio/utilities/ImageIOUtilities.java
@@ -629,52 +629,64 @@ public class ImageIOUtilities {
 	    return name;
 	}
 
-	/**
-	 * Takes a URL and converts it to a File. The attempts to deal with 
-	 * Windows UNC format specific problems, specifically files located
-	 * on network shares and different drives.
-	 * 
-	 * If the URL.getAuthority() returns null or is empty, then only the
-	 * url's path property is used to construct the file. Otherwise, the
-	 * authority is prefixed before the path.
-	 * 
-	 * It is assumed that url.getProtocol returns "file".
-	 * 
-	 * Authority is the drive or network share the file is located on.
-	 * Such as "C:", "E:", "\\fooServer"
-	 * 
-	 * @param url a URL object that uses protocol "file"
-	 * @return a File that corresponds to the URL's location
-	 */
-	public static File urlToFile(URL url) {
-	    String string = url.toExternalForm();
-	
-	    try {
-	        string = URLDecoder.decode(string, "UTF-8");
-	    } catch (UnsupportedEncodingException e) {
-	        // Shouldn't happen
-	    }
-	    
-	    String path3;
-	    String simplePrefix = "file:/";
-	    String standardPrefix = simplePrefix+"/";
-	    
-	    if( string.startsWith(standardPrefix) ){
-	        path3 = string.substring(standardPrefix.length());
-	    } else if( string.startsWith(simplePrefix)){
-	        path3 = string.substring(simplePrefix.length()-1);            
-	    } else {
-	        String auth = url.getAuthority();
-	        String path2 = url.getPath().replace("%20", " ");
-	        if (auth != null && !auth.equals("")) {
-	            path3 = "//" + auth + path2;
-	        } else {
-	            path3 = path2;
-	        }
-	    }
-	    
-	    return new File(path3);
-	}
+    /**
+     * Takes a URL and converts it to a File. The attempts to deal with 
+     * Windows UNC format specific problems, specifically files located
+     * on network shares and different drives.
+     * 
+     * If the URL.getAuthority() returns null or is empty, then only the
+     * url's path property is used to construct the file. Otherwise, the
+     * authority is prefixed before the path.
+     * 
+     * It is assumed that url.getProtocol returns "file".
+     * 
+     * Authority is the drive or network share the file is located on.
+     * Such as "C:", "E:", "\\fooServer"
+     * 
+     * @param url a URL object that uses protocol "file"
+     * @return a File that corresponds to the URL's location
+     */
+    public static File urlToFile(URL url) {
+        if (!"file".equals(url.getProtocol())) {
+            return null; // not a File URL
+        }
+        String string = url.toExternalForm();
+        if (string.contains("+")) {
+            // this represents an invalid URL created using either
+            // file.toURL(); or
+            // file.toURI().toURL() on a specific version of Java 5 on Mac
+            string = string.replace("+", "%2B");
+        }
+        try {
+            string = URLDecoder.decode(string, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Could not decode the URL to UTF-8 format", e);
+        }
+        
+        String path3;
+        String simplePrefix = "file:/";
+        String standardPrefix = "file://";
+        String os = System.getProperty("os.name");
+
+        if (os.toUpperCase().contains("WINDOWS") && string.startsWith(standardPrefix)) {
+            // win32: host/share reference
+            path3 = string.substring(standardPrefix.length() - 2);
+        } else if (string.startsWith(standardPrefix)) {
+            path3 = string.substring(standardPrefix.length());
+        } else if (string.startsWith(simplePrefix)) {
+            path3 = string.substring(simplePrefix.length() - 1);
+        } else {
+            String auth = url.getAuthority();
+            String path2 = url.getPath().replace("%20", " ");
+            if (auth != null && !auth.equals("")) {
+                path3 = "//" + auth + path2;
+            } else {
+                path3 = path2;
+            }
+        }
+
+        return new File(path3);
+    }
 
 	/**
 	 * Given a pair of xSubsamplingFactor (xSSF) and ySubsamplingFactor (ySFF), 
