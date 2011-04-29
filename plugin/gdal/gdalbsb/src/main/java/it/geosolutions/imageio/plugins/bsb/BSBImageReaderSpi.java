5290211c6f8fc1619cb95106574dbd4127072266
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2011-04-29 09:40:49 +0000

    adding support to GDAL BSB driver
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@1255 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdal/gdalbsb/src/main/java/it/geosolutions/imageio/plugins/bsb/BSBImageReaderSpi.java
@@ -0,0 +1,125 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    http://java.net/projects/imageio-ext/
+ *    (C) 2007 - 2011, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.bsb;
+
+import it.geosolutions.imageio.gdalframework.GDALImageReaderSpi;
+import it.geosolutions.imageio.stream.input.FileImageInputStreamExt;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageReader;
+
+/**
+ * Service provider interface for BSB image reader
+ * 
+ * @author Julien Carlos, INT.
+ */
+public class BSBImageReaderSpi extends GDALImageReaderSpi {
+    private static final Logger LOGGER = Logger.getLogger("it.geosolutions.imageio.plugins.bsb");
+
+    static final String[] suffixes = { "kap", "cap" };
+
+    static final String[] formatNames = { "BSB" };
+
+    static final String[] MIMETypes = { "image/kap", "image/cap" };
+
+    static final String version = "1.0";
+    
+    static final String description = "BSB Image Reader, version " + version;
+
+    static final String readerCN = "it.geosolutions.imageio.plugins.bsb.BSBImageReader";
+
+    static final String vendorName = "INT";
+
+    // writerSpiNames
+    static final String[] wSN = { null };
+
+ // StreamMetadataFormatNames and StreamMetadataFormatClassNames
+    static final boolean supportsStandardStreamMetadataFormat = false;
+
+    static final String nativeStreamMetadataFormatName = null;
+
+    static final String nativeStreamMetadataFormatClassName = null;
+
+    static final String[] extraStreamMetadataFormatNames = { null };
+
+    static final String[] extraStreamMetadataFormatClassNames = { null };
+
+    // ImageMetadataFormatNames and ImageMetadataFormatClassNames
+    static final boolean supportsStandardImageMetadataFormat = false;
+
+    static final String nativeImageMetadataFormatName = null;
+
+    static final String nativeImageMetadataFormatClassName = null;
+
+    static final String[] extraImageMetadataFormatNames = { null };
+
+    static final String[] extraImageMetadataFormatClassNames = { null };
+
+    public BSBImageReaderSpi() {
+        super(
+                vendorName,
+                version,
+                formatNames,
+                suffixes,
+                MIMETypes,
+                readerCN, // readerClassName
+                new Class[] { File.class, FileImageInputStreamExt.class },
+                wSN, // writer Spi Names
+                supportsStandardStreamMetadataFormat,
+                nativeStreamMetadataFormatName,
+                nativeStreamMetadataFormatClassName,
+                extraStreamMetadataFormatNames,
+                extraStreamMetadataFormatClassNames,
+                supportsStandardImageMetadataFormat,
+                nativeImageMetadataFormatName,
+                nativeImageMetadataFormatClassName,
+                extraImageMetadataFormatNames,
+                extraImageMetadataFormatClassNames, Collections.singletonList("BSB"));
+        
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("BSBImageReaderSpi Constructor");
+    }
+
+    /**
+     * This method checks if the provided input can be decoded from this SPI
+     */
+    public boolean canDecodeInput(Object input) throws IOException {
+        return super.canDecodeInput(input);
+    }
+
+    /**
+     * Returns an instance of the BSBImageReader
+     * 
+     * @see javax.imageio.spi.ImageReaderSpi#createReaderInstance(java.lang.Object)
+     */
+    public ImageReader createReaderInstance(Object source) throws IOException {
+        return new BSBImageReader(this);
+    }
+
+    /**
+     * @see javax.imageio.spi.IIOServiceProvider#getDescription(java.util.Locale)
+     */
+    public String getDescription(Locale locale) {
+        return description;
+    }
+}
