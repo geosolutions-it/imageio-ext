0075add22a4c65eea9aadfa2ccce7544274fc767
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2011-06-14 15:50:09 +0000

    IMAGEIO_EXT-26: CADRG RPFTOC Support
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/trunk@1261 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdal/gdalrpftoc/src/main/java/it/geosolutions/imageio/plugins/rpftoc/RPFTOCImageReaderSpi.java
@@ -0,0 +1,133 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    http://java.net/projects/imageio-ext/
+ *    (C) 2007 - 2009, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.rpftoc;
+
+import it.geosolutions.imageio.gdalframework.GDALImageReaderSpi;
+import it.geosolutions.imageio.stream.input.FileImageInputStreamExt;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageReader;
+
+/**
+ * Service provider interface for RPFTOC image reader
+ * 
+ * @author Daniele Romagnoli, GeoSolutions.
+ * @author Simone Giannecchini, GeoSolutions.
+ * @author Erik Konijnenburg
+ */
+public class RPFTOCImageReaderSpi extends GDALImageReaderSpi {
+
+    private static final Logger LOGGER = Logger
+            .getLogger("it.geosolutions.imageio.plugins.rpftoc");
+
+    // suffixes are unspecified due to the wide number of file extensions (>100)
+    // related to RPFTOC files.
+    static final String[] suffixes = { "TOC" };
+
+    static final String[] formatNames = { "RPFTOC" };
+
+    static final String[] MIMETypes = { "image/rpftoc" };
+
+    static final String version = "1.0";
+    
+    static final String description = "RPFTOC Image Reader, version " + version;
+
+    static final String readerCN = "it.geosolutions.imageio.plugins.rpftoc.RPFTOCImageReader";
+
+    static final String vendorName = "GeoSolutions";
+
+    // writerSpiNames
+    static final String[] wSN = { null };
+
+    // StreamMetadataFormatNames and StreamMetadataFormatClassNames
+    static final boolean supportsStandardStreamMetadataFormat = false;
+
+    static final String nativeStreamMetadataFormatName = null;
+
+    static final String nativeStreamMetadataFormatClassName = null;
+
+    static final String[] extraStreamMetadataFormatNames = { null };
+
+    static final String[] extraStreamMetadataFormatClassNames = { null };
+
+    // ImageMetadataFormatNames and ImageMetadataFormatClassNames
+    static final boolean supportsStandardImageMetadataFormat = false;
+
+    static final String nativeImageMetadataFormatName = null;
+
+    static final String nativeImageMetadataFormatClassName = null;
+
+    static final String[] extraImageMetadataFormatNames = { null };
+
+    static final String[] extraImageMetadataFormatClassNames = { null };
+
+    public RPFTOCImageReaderSpi() {
+        super(
+                vendorName,
+                version,
+                formatNames,
+                suffixes,
+                MIMETypes,
+                readerCN, // readerClassName
+                new Class[] { File.class, FileImageInputStreamExt.class },
+                wSN, // writer Spi Names
+                supportsStandardStreamMetadataFormat,
+                nativeStreamMetadataFormatName,
+                nativeStreamMetadataFormatClassName,
+                extraStreamMetadataFormatNames,
+                extraStreamMetadataFormatClassNames,
+                supportsStandardImageMetadataFormat,
+                nativeImageMetadataFormatName,
+                nativeImageMetadataFormatClassName,
+                extraImageMetadataFormatNames,
+                extraImageMetadataFormatClassNames, Collections
+                        .singletonList("RPFTOC"));
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("RPFTOCImageReaderSpi Constructor");
+
+    }
+
+    /**
+     * This method checks if the provided input can be decoded from this SPI
+     */
+    public boolean canDecodeInput(Object input) throws IOException {
+        return super.canDecodeInput(input);
+    }
+
+    /**
+     * Returns an instance of the RPFTOCImageReader
+     * 
+     * @see javax.imageio.spi.ImageReaderSpi#createReaderInstance(java.lang.Object)
+     */
+    public ImageReader createReaderInstance(Object source) throws IOException {
+        return new RPFTOCImageReader(this);
+    }
+
+    /**
+     * @see javax.imageio.spi.IIOServiceProvider#getDescription(java.util.Locale)
+     */
+    public String getDescription(Locale locale) {
+        return description;
+    }
+
+}
