2a05df58b8248e7f3cd38cf4fc2766cd52d80275
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2011-07-18 22:48:15 +0000

    IMAGEIO_EXT-27
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/branches/1.1.x@1273 866bbf7d-4862-ccf5-812a-c90ffd63a74d

---
+++ plugin/gdal/gdalidrisi/src/main/java/it/geosolutions/imageio/plugins/idrisi/IDRISIImageReader.java
@@ -0,0 +1,44 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    http://java.net/projects/imageio-ext/
+ *    (C) 2007 - 2009, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package it.geosolutions.imageio.plugins.idrisi;
+
+import it.geosolutions.imageio.gdalframework.GDALImageReader;
+
+import java.awt.image.RenderedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * {@link IDRISIImageReader} is a {@link GDALImageReader} able to create
+ * {@link RenderedImage} from ASCII arcGrid files.
+ * 
+ * @author Simone Giannecchini, GeoSolutions.
+ * @author Daniele Romagnoli, GeoSolutions.
+ * 
+ */
+public class IDRISIImageReader extends GDALImageReader {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(IDRISIImageReader.class.toString());
+
+	public IDRISIImageReader(IDRISIImageReaderSpi originatingProvider) {
+		super(originatingProvider,0);
+		if (LOGGER.isLoggable(Level.FINE))
+			LOGGER.fine("IDRISIImageReader Constructor");
+	}
+
+}
