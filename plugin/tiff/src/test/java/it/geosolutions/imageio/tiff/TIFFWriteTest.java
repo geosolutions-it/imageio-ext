bcd449ec3c07ed146b7d622aa576db9ed950d271
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2011-10-21 14:57:59 +0000

    aligning tests
    git-svn-id: https://svn.java.net/svn/imageio-ext~svn/branches/1.1.x@1301 866bbf7d-4862-ccf5-812a-c90ffd63a74d

--- plugin/tiff/src/test/java/it/geosolutions/imageio/tiff/TIFFWriteTest.java
+++ plugin/tiff/src/test/java/it/geosolutions/imageio/tiff/TIFFWriteTest.java
@@ -15,9 +15,12 @@
  */
 package it.geosolutions.imageio.tiff;
 
+import it.geosolutions.imageio.plugins.tiff.TIFFImageWriteParam;
 import it.geosolutions.imageio.utilities.ImageIOUtilities;
 import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageReader;
 import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageReaderSpi;
+import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageWriter;
+import it.geosolutions.imageioimpl.plugins.tiff.TIFFImageWriterSpi;
 import it.geosolutions.resources.TestData;
 
 import java.awt.Rectangle;
@@ -25,15 +28,24 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
+import java.util.logging.Logger;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageReadParam;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.event.IIOWriteProgressListener;
 import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.media.jai.PlanarImage;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.sun.media.jai.operator.ImageReadDescriptor;
+
 
 /**
  * Testing reading capabilities for {@link JP2KKakaduImageReader} leveraging on JAI.
@@ -41,9 +53,12 @@ import com.sun.media.jai.operator.ImageReadDescriptor;
  * @author Simone Giannecchini, GeoSolutions.
  * @author Daniele Romagnoli, GeoSolutions.
  */
-public class TIFFReadTest extends Assert {
+public class TIFFWriteTest extends Assert {
+    
+    private final static Logger LOGGER = Logger.getLogger(TIFFWriteTest.class.toString());
 
     @Test
+    @Ignore
     public void readFromFileJAI() throws IOException {
         final File file = TestData.file(this, "test.tif");
 
@@ -185,75 +200,97 @@ public class TIFFReadTest extends Assert {
     }
 
     @Test
-    public void readFromFileDirect() throws IOException {
+    public void readWriteFromFileDirect() throws IOException {
 
-        final File file = TestData.file(this, "test.tif");
+//        JAI.getDefaultInstance().getTileCache().setMemoryCapacity(512*1024*1024);
+//        final TCTool tc= new TCTool((SunTileCache)JAI.getDefaultInstance().getTileCache());
+        final File inputFile =TestData.file(this, "test.tif");// new File("c:\\work\\dem30_final.tiff");
+        final File outputFile = TestData.temp(this, "testw.tif",true);
+        
+        
 
         final ImageReadParam param = new ImageReadParam();
         param.setSourceRegion(new Rectangle(0, 0, 10, 10));
 
-        // double sum=0;
-        // final long num = 10000l;
 
-        final TIFFImageReader reader = (TIFFImageReader) new TIFFImageReaderSpi()
+
+        TIFFImageReader reader = (TIFFImageReader) new TIFFImageReaderSpi()
                 .createReaderInstance();
-        reader.setInput(new FileImageInputStream(file));
-        // System.out.println(new IIOMetadataDumper(
-        // reader.getImageMetadata(0),TIFFImageMetadata.nativeMetadataFormatName).getMetadata());
+        reader.setInput(new FileImageInputStream(inputFile));
 
-        // for(long i=0;i<num;i++){
-        // final double time= System.nanoTime();
-        // IMAGE 0
         BufferedImage image = reader.read(0, param);
+//        RenderedImage image = ImageReadDescriptor.create(new FileImageInputStream(inputFile),
+//                Integer.valueOf(0), false, false, false, null, null, null,
+//                reader, new RenderingHints(JAI.KEY_IMAGE_LAYOUT, new ImageLayout().setTileGridXOffset(0).setTileGridYOffset(0).setTileHeight(512).setTileWidth(512)));
+        
+        final TIFFImageWriter writer= (TIFFImageWriter) new TIFFImageWriterSpi().createWriterInstance();
+        final ImageWriteParam writeParam= new TIFFImageWriteParam(Locale.getDefault());
+        writeParam.setTilingMode(ImageWriteParam.MODE_EXPLICIT);
+        writeParam.setTiling(512, 512,0,0);
+
+        
+//        for( int i=0;i<1;i++){
+        writer.setOutput(new FileImageOutputStream(outputFile));
+        
+        // BUG
+//        writer.write(metadata, new IIOImage(image, null, metadata), null);
+        writer.addIIOWriteProgressListener(new IIOWriteProgressListener() {
+            
+            public void writeAborted(ImageWriter source) {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            public void thumbnailStarted(ImageWriter source, int imageIndex, int thumbnailIndex) {
+                assertTrue(imageIndex>=0);
+                assertTrue(thumbnailIndex==0);
+                
+            }
+            
+            public void thumbnailProgress(ImageWriter source, float percentageDone) {
+                assertTrue(percentageDone>=0&&percentageDone<=100);
+                
+            }
+            
+            public void thumbnailComplete(ImageWriter source) {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            public void imageStarted(ImageWriter source, int imageIndex) {
+                assertTrue(imageIndex>=0);
+                LOGGER.info("imageStarted");
+                
+            }
+            
+            public void imageProgress(ImageWriter source, float percentageDone) {
+                assertTrue(percentageDone>=0&&percentageDone<=100);
+                LOGGER.info(percentageDone+"%");
+                
+            }
+            
+            public void imageComplete(ImageWriter source) {
+                LOGGER.info("imageComplete");
+                
+            }
+        });
+        writer.write(null,new IIOImage(image, null,null),writeParam);
+        
+//        }
+        writer.dispose();
+        
+        
+        
+        reader.reset();
+        reader.setInput(new FileImageInputStream(outputFile));
+        image = reader.read(0);
+        
+               
         Assert.assertEquals(10, image.getWidth());
         Assert.assertEquals(10, image.getHeight());
         image.flush();
-        image = null;
-
-        image = reader.read(1, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        image = reader.read(2, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        image = reader.read(1, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        image = reader.read(3, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        image = reader.read(0, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        image = reader.read(0, param);
-        Assert.assertEquals(10, image.getWidth());
-        Assert.assertEquals(10, image.getHeight());
-        image.flush();
-        image = null;
-
-        // sum+=System.nanoTime()-time;
-        // Assert.assertEquals(120, image.getWidth());
-        // Assert.assertEquals(107, image.getHeight());
-
-        // System.out.println("test "+i);
-        //
-        // }
-        // System.out.println(sum/num);
+        image=null;
+        
         reader.dispose();
     }
 }
