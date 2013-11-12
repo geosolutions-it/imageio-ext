1d99e049b915a835759322d63502795c8d9c65d5
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2013-11-12 17:56:33 +0100

    [PAMSupport]: adding utility method to parse metadata

--- library/geocore/src/main/java/it/geosolutions/imageio/pam/PAMParser.java
+++ library/geocore/src/main/java/it/geosolutions/imageio/pam/PAMParser.java
@@ -1,6 +1,12 @@
 package it.geosolutions.imageio.pam;
 
+import it.geosolutions.imageio.pam.PAMDataset.PAMRasterBand;
+import it.geosolutions.imageio.pam.PAMDataset.PAMRasterBand.Metadata;
+import it.geosolutions.imageio.pam.PAMDataset.PAMRasterBand.Metadata.MDI;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,13 +44,18 @@ public class PAMParser {
         }
     }
 
-    public PAMDataset parsePAM(final String filePath) throws JAXBException {
+    public PAMDataset parsePAM(final String filePath) throws IOException {
         final File file = new File(filePath);
         return parsePAM(file);
     }
 
-    public PAMDataset parsePAM(final File file) throws JAXBException {
-        PAMDataset pamDataset = unmarshal(file);
+    public PAMDataset parsePAM(final File file) throws IOException {
+        PAMDataset pamDataset;
+        try {
+            pamDataset = unmarshal(file);
+        } catch (JAXBException e) {
+            throw new IOException("Exception occurred while parsing the file", e);
+        }
         return pamDataset;
     }
 
@@ -63,5 +74,35 @@ public class PAMParser {
             pamDataset = (PAMDataset) unmarshaller.unmarshal(pamFile);
         }
         return pamDataset;
+    }
+    
+    /**
+     * Return the value of a metadata string for the provided rasterBand.
+     * @param rasterBand the {@link PAMRasterBand} to be checked.
+     * @param key The key of the metadata property to be retrieved
+     * @return
+     */
+    public String getMetadataValue (final PAMRasterBand rasterBand, final String key) {
+        if (rasterBand == null) {
+            throw new IllegalArgumentException("Specified raster band is null");
+        }
+        Metadata metadata = rasterBand.getMetadata();
+        if (metadata == null) {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("No metadata have been found from the provided Raster band");
+            }
+            return null;
+        }
+        List<MDI> mdis = metadata.getMDI();
+        if (mdis == null || mdis.size() == 0)
+        if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("No metadata have been found from the provided Raster band");
+        }
+        for (MDI mdi: mdis) {
+            if (mdi.getKey().equalsIgnoreCase(key)) {
+                return mdi.getValue();
+            }
+        }
+        return null;
     }
 }
