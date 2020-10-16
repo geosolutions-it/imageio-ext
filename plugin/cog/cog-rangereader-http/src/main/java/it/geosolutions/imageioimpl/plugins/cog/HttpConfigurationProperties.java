82d9665e0413960f1bc2a813a78128e9d107e9cf
Daniele Romagnoli <daniele.romagnoli@geo-solutions.it>
2020-10-16 11:37:17 +0200

    Adding headers where missing + http config and s3config env checks in the same order

--- plugin/cog/cog-rangereader-http/src/main/java/it/geosolutions/imageioimpl/plugins/cog/HttpConfigurationProperties.java
+++ plugin/cog/cog-rangereader-http/src/main/java/it/geosolutions/imageioimpl/plugins/cog/HttpConfigurationProperties.java
@@ -1,3 +1,19 @@
+/*
+ *    ImageI/O-Ext - OpenSource Java Image translation Library
+ *    http://www.geo-solutions.it/
+ *    http://java.net/projects/imageio-ext/
+ *    (C) 2019, GeoSolutions
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    either version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package it.geosolutions.imageioimpl.plugins.cog;
 
 /**
@@ -22,13 +38,13 @@ public class HttpConfigurationProperties {
 
     public HttpConfigurationProperties() {
         maxRequests = Integer.parseInt(
-                PropertyLocator.getPropertyValue(HTTP_MAX_REQUESTS, "128"));
+                PropertyLocator.getEnvironmentValue(HTTP_MAX_REQUESTS, "128"));
         maxRequestsPerHost = Integer.parseInt(
-                PropertyLocator.getPropertyValue(HTTP_MAX_REQUESTS_PER_HOST, "5"));
+                PropertyLocator.getEnvironmentValue(HTTP_MAX_REQUESTS_PER_HOST, "5"));
         maxIdleConnections = Integer.parseInt(
-                PropertyLocator.getPropertyValue(HTTP_MAX_IDLE_CONNECTIONS, "5"));
+                PropertyLocator.getEnvironmentValue(HTTP_MAX_IDLE_CONNECTIONS, "5"));
         keepAliveDuration = Integer.parseInt(
-                PropertyLocator.getPropertyValue(HTTP_KEEP_ALIVE_TIME, "60"));
+                PropertyLocator.getEnvironmentValue(HTTP_KEEP_ALIVE_TIME, "60"));
     }
 
     public int getMaxRequests() {
