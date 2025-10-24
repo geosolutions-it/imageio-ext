8cf6b4c80f39e6e4eff8536621f8c2f49025dd73
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-10-24 14:12:04 -0300

    Apply formatting to turbojpeg library

--- library/libjpegturbo/src/main/java/org/libjpegturbo/turbojpeg/TJCustomFilter.java
+++ library/libjpegturbo/src/main/java/org/libjpegturbo/turbojpeg/TJCustomFilter.java
@@ -31,46 +31,36 @@ package org.libjpegturbo.turbojpeg;
 import java.awt.*;
 import java.nio.*;
 
-/**
- * Custom filter callback interface
- */
+/** Custom filter callback interface */
 public interface TJCustomFilter {
 
-  /**
-   * A callback function that can be used to modify the DCT coefficients after
-   * they are losslessly transformed but before they are transcoded to a new
-   * JPEG file.  This allows for custom filters or other transformations to be
-   * applied in the frequency domain.
-   *
-   * @param coeffBuffer a buffer containing transformed DCT coefficients.
-   * (NOTE: this buffer is not guaranteed to be valid once the callback
-   * returns, so applications wishing to hand off the DCT coefficients to
-   * another function or library should make a copy of them within the body of
-   * the callback.)
-   *
-   * @param bufferRegion rectangle containing the width and height of
-   * <code>coeffBuffer</code> as well as its offset relative to the component
-   * plane.  TurboJPEG implementations may choose to split each component plane
-   * into multiple DCT coefficient buffers and call the callback function once
-   * for each buffer.
-   *
-   * @param planeRegion rectangle containing the width and height of the
-   * component plane to which <code>coeffBuffer</code> belongs
-   *
-   * @param componentID ID number of the component plane to which
-   * <code>coeffBuffer</code>belongs (Y, Cb, and Cr have, respectively, ID's of
-   * 0, 1, and 2 in typical JPEG images.)
-   *
-   * @param transformID ID number of the transformed image to which
-   * <code>coeffBuffer</code> belongs.  This is the same as the index of the
-   * transform in the transforms array that was passed to {@link
-   * TJTransformer#transform TJTransformer.transform()}.
-   *
-   * @param transform a {@link TJTransform} instance that specifies the
-   * parameters and/or cropping region for this transform
-   */
-  public void customFilter(ShortBuffer coeffBuffer, Rectangle bufferRegion,
-    Rectangle planeRegion, int componentID, int transformID,
-    TJTransform transform)
-    throws Exception;
+    /**
+     * A callback function that can be used to modify the DCT coefficients after they are losslessly transformed but
+     * before they are transcoded to a new JPEG file. This allows for custom filters or other transformations to be
+     * applied in the frequency domain.
+     *
+     * @param coeffBuffer a buffer containing transformed DCT coefficients. (NOTE: this buffer is not guaranteed to be
+     *     valid once the callback returns, so applications wishing to hand off the DCT coefficients to another function
+     *     or library should make a copy of them within the body of the callback.)
+     * @param bufferRegion rectangle containing the width and height of <code>coeffBuffer</code> as well as its offset
+     *     relative to the component plane. TurboJPEG implementations may choose to split each component plane into
+     *     multiple DCT coefficient buffers and call the callback function once for each buffer.
+     * @param planeRegion rectangle containing the width and height of the component plane to which <code>coeffBuffer
+     *     </code> belongs
+     * @param componentID ID number of the component plane to which <code>coeffBuffer</code>belongs (Y, Cb, and Cr have,
+     *     respectively, ID's of 0, 1, and 2 in typical JPEG images.)
+     * @param transformID ID number of the transformed image to which <code>coeffBuffer</code> belongs. This is the same
+     *     as the index of the transform in the transforms array that was passed to {@link TJTransformer#transform
+     *     TJTransformer.transform()}.
+     * @param transform a {@link TJTransform} instance that specifies the parameters and/or cropping region for this
+     *     transform
+     */
+    public void customFilter(
+            ShortBuffer coeffBuffer,
+            Rectangle bufferRegion,
+            Rectangle planeRegion,
+            int componentID,
+            int transformID,
+            TJTransform transform)
+            throws Exception;
 }
--- library/libjpegturbo/src/main/java/org/libjpegturbo/turbojpeg/TJScalingFactor.java
+++ library/libjpegturbo/src/main/java/org/libjpegturbo/turbojpeg/TJScalingFactor.java
@@ -28,71 +28,67 @@
 
 package org.libjpegturbo.turbojpeg;
 
-/**
- * Fractional scaling factor
- */
+/** Fractional scaling factor */
 public class TJScalingFactor {
 
-  public TJScalingFactor(int num, int denom) throws Exception {
-    if(num < 1 || denom < 1)
-      throw new Exception("Numerator and denominator must be >= 1");
-    this.num = num;
-    this.denom = denom;
-  }
+    public TJScalingFactor(int num, int denom) throws Exception {
+        if (num < 1 || denom < 1) throw new Exception("Numerator and denominator must be >= 1");
+        this.num = num;
+        this.denom = denom;
+    }
 
-  /**
-   * Returns numerator
-   * @return numerator
-   */
-  public int getNum() {
-    return num;
-  }
+    /**
+     * Returns numerator
+     *
+     * @return numerator
+     */
+    public int getNum() {
+        return num;
+    }
 
-  /**
-   * Returns denominator
-   * @return denominator
-   */
-  public int getDenom() {
-    return denom;
-  }
+    /**
+     * Returns denominator
+     *
+     * @return denominator
+     */
+    public int getDenom() {
+        return denom;
+    }
 
-  /**
-   * Returns the scaled value of <code>dimension</code>.  This function
-   * performs the integer equivalent of
-   * <code>ceil(dimension * scalingFactor)</code>.
-   * @return the scaled value of <code>dimension</code>
-   */
-  public int getScaled(int dimension) {
-    return (dimension * num + denom - 1) / denom;
-  }
+    /**
+     * Returns the scaled value of <code>dimension</code>. This function performs the integer equivalent of <code>
+     * ceil(dimension * scalingFactor)</code>.
+     *
+     * @return the scaled value of <code>dimension</code>
+     */
+    public int getScaled(int dimension) {
+        return (dimension * num + denom - 1) / denom;
+    }
 
-  /**
-   * Returns true or false, depending on whether this instance and
-   * <code>other</code> have the same numerator and denominator.
-   * @return true or false, depending on whether this instance and
-   * <code>other</code> have the same numerator and denominator
-   */
-  public boolean equals(TJScalingFactor other) {
-    return (this.num == other.num && this.denom == other.denom);
-  }
+    /**
+     * Returns true or false, depending on whether this instance and <code>other</code> have the same numerator and
+     * denominator.
+     *
+     * @return true or false, depending on whether this instance and <code>other</code> have the same numerator and
+     *     denominator
+     */
+    public boolean equals(TJScalingFactor other) {
+        return (this.num == other.num && this.denom == other.denom);
+    }
 
-  /**
-   * Returns true or false, depending on whether this instance is equal to
-   * 1/1.
-   * @return true or false, depending on whether this instance is equal to
-   * 1/1
-   */
-  public boolean isOne() {
-    return (num == 1 && denom == 1);
-  }
+    /**
+     * Returns true or false, depending on whether this instance is equal to 1/1.
+     *
+     * @return true or false, depending on whether this instance is equal to 1/1
+     */
+    public boolean isOne() {
+        return (num == 1 && denom == 1);
+    }
 
-  /**
-   * Numerator
-   */
-  private int num = 1;
+    /** Numerator */
+    private int num = 1;
 
-  /**
-   * Denominator
-   */
-  private int denom = 1;
-};
+    /** Denominator */
+    private int denom = 1;
+}
+;
