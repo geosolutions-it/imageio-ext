5a124fdfcb7b9ffd79b1c8fbbf42f2773a0af55a
Andrea Aime <andrea.aime@gmail.com>
2026-09-15 13:02:39 +0200

    Update RAT support to include new GDAL 3.12+ field types

--- plugin/gdal/gdalvrt/src/test/java/it/geosolutions/imageio/plugins/vrt/RasterAttributeTableTest.java
+++ plugin/gdal/gdalvrt/src/test/java/it/geosolutions/imageio/plugins/vrt/RasterAttributeTableTest.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertTrue;
 import it.geosolutions.imageio.gdalframework.AbstractGDALTest;
 import it.geosolutions.imageio.gdalframework.GDALCommonIIOImageMetadata;
 import it.geosolutions.imageio.pam.PAMDataset;
+import it.geosolutions.imageio.pam.PAMDataset.PAMRasterBand.TableType;
 import it.geosolutions.resources.TestData;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -64,6 +65,7 @@ public class RasterAttributeTableTest extends AbstractGDALTest {
         PAMDataset.PAMRasterBand band = pam.getPAMRasterBand().get(0);
         PAMDataset.PAMRasterBand.GDALRasterAttributeTable rat = band.getGdalRasterAttributeTable();
         assertNotNull(rat);
+        assertEquals(TableType.Thematic, rat.getTableType());
 
         // Check each field
         List<PAMDataset.PAMRasterBand.FieldDefn> fields = rat.getFieldDefn();
@@ -96,6 +98,58 @@ public class RasterAttributeTableTest extends AbstractGDALTest {
         assertEquals("white", fieldValues.get(2));
 
         reader.dispose();
+    }
+
+    @Test
+    public void readGdal312FieldTypes() throws FileNotFoundException, IOException {
+        if (!isGDALAvailable) {
+            return;
+        }
+        final File file = TestData.file(this, "rat-gdal312-types.vrt");
+
+        final ImageReader reader = ImageIO.getImageReaders(file).next();
+        reader.setInput(file);
+        try {
+            GDALCommonIIOImageMetadata gdalMeta = (GDALCommonIIOImageMetadata) reader.getImageMetadata(0);
+            PAMDataset.PAMRasterBand.GDALRasterAttributeTable rat =
+                    gdalMeta.getPamDataset().getPAMRasterBand().get(0).getGdalRasterAttributeTable();
+            assertNotNull(rat);
+            assertEquals(TableType.Thematic, rat.getTableType());
+
+            List<PAMDataset.PAMRasterBand.FieldDefn> fields = rat.getFieldDefn();
+            assertEquals(8, fields.size());
+            assertField(
+                    fields.get(2),
+                    "fullSeafloorCoverageAchieved",
+                    PAMDataset.PAMRasterBand.FieldType.Boolean,
+                    PAMDataset.PAMRasterBand.FieldUsage.Generic);
+            assertField(
+                    fields.get(3),
+                    "surveyDateRange.dateStart",
+                    PAMDataset.PAMRasterBand.FieldType.DateTime,
+                    PAMDataset.PAMRasterBand.FieldUsage.Generic);
+            assertField(
+                    fields.get(7),
+                    "footprint",
+                    PAMDataset.PAMRasterBand.FieldType.WKBGeometry,
+                    PAMDataset.PAMRasterBand.FieldUsage.Generic);
+
+            List<PAMDataset.PAMRasterBand.Row> rows = rat.getRow();
+            assertEquals(3, rows.size());
+            assertEquals(
+                    List.of(
+                            "54602",
+                            "1",
+                            "true",
+                            "2024-03-01T00:00:00.000+00:00",
+                            "2024-03-14T23:59:59.000+00:00",
+                            "NOAA",
+                            "4.75",
+                            "POLYGON ((0 0,1 0,1 1,0 1,0 0))"),
+                    rows.get(0).getF());
+        } finally {
+            reader.dispose();
+        }
     }
 
     private void assertField(
